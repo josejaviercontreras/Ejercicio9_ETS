896e249652ca5bbc4cc5e77aef1ba6b0f407e9de
Jose Javier Contreras Pestano <josejaviercontreraspestano@gmail.com>
2024-04-30 15:58:00 +0100

    Sumar
    crear función sumar usando lambda

--- Ejercicio9/Funciones.cs
+++ Ejercicio9/Funciones.cs
@@ -8,7 +8,7 @@ namespace Ejercicio9
 {
     internal class Funciones
     {
-        // public static int sumar(int a, int b)
+         public static int sumar(int a, int b) => a + b;
         // public static int multiplicar(int a, int b)
         // public static int dividir(int a, int b)
         // public static int restar(int a, int b)
